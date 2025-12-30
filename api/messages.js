4253d45139bd45adb7fd02e20f03d2c9e42910b3
saad khan <86378917+saadkhanG@users.noreply.github.com>
2025-12-31 02:39:11 +0500

    Update messages.js

--- api/messages.js
+++ api/messages.js
@@ -19,6 +19,6 @@ export default async function handler(req, res) {
 
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({ error: "Database error" });
+    res.status(500).json({ error: err.message });
   }
 }
